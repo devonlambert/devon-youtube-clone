603c33751c1fc634282666193c2b2b423787bc02
Devon Lambert <devon@realityknights.com>
2019-10-12 11:40:39 -0500

    Moves Sass to root AND Adds 7-1 architecture to SASS folder

--- app.js
+++ app.js
@@ -20,10 +20,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(sassMiddleware({
-  src: path.join(__dirname, 'public'),
-  dest: path.join(__dirname, 'public'),
-  indentedSyntax: false, // true = .sass and false = .scss
-  sourceMap: false
+  src: path.join(__dirname, 'sass'),
+  dest: path.join(__dirname, 'public/stylesheets'),
+  prefix: '/stylesheets',
+  debug: true,
+  force: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
 
